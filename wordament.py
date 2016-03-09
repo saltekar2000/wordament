3c45a394026a9322e58b2d23f18f74f129535cf8
Shirish Altekar <saltekar@yahoo.com>
2016-03-08 23:12:48 -0800

    minor edits

--- wordament.py
+++ wordament.py
@@ -3,7 +3,7 @@ from bisect import bisect_right
 from copy import deepcopy
 
 GRIDSIZE = 4
-DEPTHBOUND = 10
+DEPTHBOUND = 16
 
 # read in standard dictionary shipped with linux
 dictionary = [word.strip() for word in open('/usr/share/dict/words') if word[0].islower() and word.find("'") == -1]  
@@ -36,21 +36,20 @@ def word_search( i,j, word, visited ):
     if found and len(word) >= 3:
 	    print word
     if prefix and len(word_copy) < DEPTHBOUND:
-        word_search(i+1,j,word_copy,visited_copy)
-        word_search(i+1,j+1,word_copy,visited_copy)
-        word_search(i,j+1,word_copy,visited_copy)
-        word_search(i-1,j+1,word_copy,visited_copy)
-        word_search(i-1,j,word_copy,visited_copy)
-        word_search(i-1,j-1,word_copy,visited_copy)
-        word_search(i,j-1,word_copy,visited_copy)
-        word_search(i+1,j-1,word_copy,visited_copy)
+        word_search( i+1, j, word_copy, visited_copy )
+        word_search( i+1, j+1, word_copy, visited_copy )
+        word_search( i, j+1, word_copy, visited_copy )
+        word_search( i-1, j+1, word_copy, visited_copy )
+        word_search( i-1, j, word_copy, visited_copy )
+        word_search( i-1, j-1, word_copy, visited_copy )
+        word_search( i, j-1, word_copy, visited_copy )
+        word_search( i+1, j-1, word_copy, visited_copy )
         		
 def grid_search( game_grid ):
     for i in range(GRIDSIZE):
         for j in range(GRIDSIZE):
             visited = np.zeros((GRIDSIZE,GRIDSIZE),dtype=np.bool)
-            word = ''
-            word_search( i,j, word, visited)
+            word_search( i, j, '', visited )
             
 grid_search(game_grid)		    
 		
